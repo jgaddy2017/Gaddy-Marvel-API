20adbade95c00b8f47ec78d33480250efbc0dcb3
Smith Gaddy <jgaddy@citadel.edu>
2018-06-18 01:51:02 -0400

    Finished Project

--- characterObject.js
+++ characterObject.js
@@ -28,31 +28,53 @@ const characterGroupObject = {
     Z: ""
 }
 
-const characterIndexingList = '<div class="characterIndexTable">\
-                                <div class="characterIndex">A</div>\
-                                <div class="characterIndex">B</div>\
-                                <div class="characterIndex">C</div>\
-                                <div class="characterIndex">D</div>\
-                                <div class="characterIndex">E</div>\
-                                <div class="characterIndex">F</div>\
-                                <div class="characterIndex">G</div>\
-                                <div class="characterIndex">H</div>\
-                                <div class="characterIndex">I</div>\
-                                <div class="characterIndex">J</div>\
-                                <div class="characterIndex">K</div>\
-                                <div class="characterIndex">L</div>\
-                                <div class="characterIndex">M</div>\
-                                <div class="characterIndex">N</div>\
-                                <div class="characterIndex">O</div>\
-                                <div class="characterIndex">P</div>\
-                                <div class="characterIndex">Q</div>\
-                                <div class="characterIndex">R</div>\
-                                <div class="characterIndex">S</div>\
-                                <div class="characterIndex">T</div>\
-                                <div class="characterIndex">U</div>\
-                                <div class="characterIndex">V</div>\
-                                <div class="characterIndex">W</div>\
-                                <div class="characterIndex">X</div>\
-                                <div class="characterIndex">Y</div>\
-                                <div class="characterIndex">Z</div>\
+const characterIndexingList = '<h4>A-Z character index based on characters first name</h4>\
+                                <div class="characterIndexTable">\
+                                <button class="characterIndex">A</button>\
+                                <button class="characterIndex">B</button>\
+                                <button class="characterIndex">C</button>\
+                                <button class="characterIndex">D</button>\
+                                <button class="characterIndex">E</button>\
+                                <button class="characterIndex">F</button>\
+                                <button class="characterIndex">G</button>\
+                                <button class="characterIndex">H</button>\
+                                <button class="characterIndex">I</button>\
+                                <button class="characterIndex">J</button>\
+                                <button class="characterIndex">K</button>\
+                                <button class="characterIndex">L</button>\
+                                <button class="characterIndex">M</button>\
+                                <button class="characterIndex">N</button>\
+                                <button class="characterIndex">O</button>\
+                                <button class="characterIndex">P</button>\
+                                <button class="characterIndex">Q</button>\
+                                <button class="characterIndex">R</button>\
+                                <button class="characterIndex">S</button>\
+                                <button class="characterIndex">T</button>\
+                                <button class="characterIndex">U</button>\
+                                <button class="characterIndex">V</button>\
+                                <button class="characterIndex">W</button>\
+                                <button class="characterIndex">X</button>\
+                                <button class="characterIndex">Y</button>\
+                                <button class="characterIndex">Z</button>\
                             </div>';
+
+let openingRemarks = `<p class="openingStatements">Thank you from visiting my <span class="importantOpeningStatement">Marvel API Application!</span><p>
+                      <p class="openingStatements">To begin: on the <span class="importantOpeningStatement">top left</span> 
+                      you will find a <span class="importantOpeningStatement">
+                      character button</span>. Clicking on this button will give you an A-Z index. Use this index to access a list of Marvel characters.
+                      Characters are listed by their first names.</p>
+                      <p class="openingStatements">Example: If you want information on <span class="importantOpeningStatement">Captain America</span>, 
+                       <span class="importantOpeningStatement">click on the "C"</span> from the index. For easier accessibily, you can also <span class="importantOpeningStatement">
+                      click the letter on your keyboard</span>.</p>
+                      <p class="openingStatements">These characters are being accessed from the <span class="importantOpeningStatement">Marvel API</span>.</p>
+                      <p class="openingStatements">You will notice that the <span class="importantOpeningStatement">character button is disabled </span>as soon as you get 
+                      to the site, thereby enabling all characters to <span class="importantOpeningStatement">load in at one time</span>. 
+                      This ensures a smoother user experience. Loading Process below:</p>
+                      <p class="dataTimer"><span id="loader">0</span> out of 26<p>
+                      <p class="openingStatements">The statistics and background of each character is accessed from the Super Hero API, so <span class="importantOpeningStatement">not all characters will have information</span></p>
+                      <p class="openingStatements">An interesting <span class="importantOpeningStatement">list of characters</span> to begin with would include:</p>
+                      <p class="importantOpeningStatement">Deadpool</p>
+                      <p class="importantOpeningStatement">Thanos</p>
+                      <p class="importantOpeningStatement">Iron Man</p>
+                      <p class="importantOpeningStatement">Cable</p>
+                      <p class="importantOpeningStatement">Darkhawk</p>`;
--- index.js
+++ index.js
@@ -1,33 +1,46 @@
 
+//I should probably find a way to hide this
 let PRIVATE_KEY = "6e8c81140cf26bea43c9feeccd70e3d759b6edf7";
 let PUBLIC_KEY = "1fed845063d20c661e374633d49e3bc0";
 
 let buttonActivation = 0;
 
-
-function fillCharacterObject(){
-
-    Object.keys(characterGroupObject).forEach(function(key){
-
-        runMarvelApi(key);
-        
-    });
-    //console.log(characterGroupObject);
-
-
+//displays to the user the update on how long it is taking to gain all character information
+function updateLoader(){
+    $('#loader').html(buttonActivation);
 }
 
-function createCharacterObject(data, startLetter){
+//runs A-Z in the alphbet to retrieve all heros at the start of the application
+function fillCharacterObject(){
 
+    return Object.keys(characterGroupObject).map(function(key){
+
+        return runMarvelApi(key);
+        
+    });
+}
+
+//this activates the character button, once all characters have been loaded into the characterGroupObject
+function whenFillCharacter(){
+    const promises = fillCharacterObject();
+    $.when(...promises).done(function(){
+        console.log("done");
+        $('#myBtn').prop('disabled', false);
+    });
+}
+
+//creates the page of all hero cards by their first names and stores those pages in the characterGroupObject
+//characterGroupObject is found in characterObject.js
+function createCharacterObject(data, startLetter){
    let card = data.data.results.map(result => createCards(result));
    let cardList = card.join("");
-   //console.log(characterGroupObject);
    characterGroupObject[startLetter] = cardList;
 }
 
 
-
-function runMarvelApi(startLetter, callback){
+//runs at the beginning to gain all characters from marvel api
+//loads all characters at once, runs A then B ..... then Z.
+function runMarvelApi(startLetter){
     let ts = new Date().getTime();
     let hash = md5(ts + PRIVATE_KEY + PUBLIC_KEY).toString();
 
@@ -41,36 +54,56 @@ function runMarvelApi(startLetter, callback){
         limit: 99
     }
 
-    $.getJSON(url, query, function(data){
+    return $.getJSON(url, query, function(data){
+        /*
+        buttonActivation is used for the button timer that 
+        is displayed to let the user know the application is ready.
+        (all heros have been extracted from the api).
+        */
         buttonActivation++;
+        updateLoader();
         createCharacterObject(data, startLetter);
-        if(buttonActivation == 26){
-            alert("ready");
-        }
-    });
-
+    }).fail(showErr);
 
 }
+//creates an error message if something is wrong with retrieving data from marvel api
+function showErr(err){
+    const errMsg = `<div class="sorryTitleSection">
+                        <p class="sorryTitle">Sorry<p>
+                    </div>
+                    <p class="sorryDescription">There was an <span class="importantOpeningStatement">error</span> retrieving data!</p>
+                    <p class="sorryDescription">Something went <span class="importantOpeningStatement">Wrong</span> 
+                    trying to connect to the <span class="importantOpeningStatement">Marvel API</span></p>
+                    <p class="sorryDescription">Try checking your <span class="importantOpeningStatement">internet connection</span> 
+                    and <span class="importantOpeningStatement">refreshing</span> the page</p>`;
+    $('#characterPage').html(errMsg);
+}
 
+//when a letter is selected display all hero cards with that letter
 function displayResults(letterIndex){
     let cards = characterGroupObject[letterIndex];
     $('#results').html(cards);
 }
 
+//creates the cards for each character that will be shown in the modal.
 function createCards(character){
-   return `<div class="card">\
-                <img src="${character.thumbnail.path}.${character.thumbnail.extension}">\
+   return `<button class="card">\
+                <img class="characterThumbnail" src="${character.thumbnail.path}.${character.thumbnail.extension}">\
                 <p>${character.name}</p>\
-            </div>`;
+            </button>`;
 }
 
+//handles the A-Z index when the user uses their keyboard to select a letter
 function handleKeyDown(){
     $('html').keydown(event => {
         let keyboardPressed = event.key;
-        runMarvelApi(keyboardPressed, displayResults);
+        console.log(keyboardPressed.toUpperCase());
+        displayResults(keyboardPressed.toUpperCase());
+        handleCardClick();
     });
 }
 
+//handles the A-Z index when the user uses the tab or mouse to click on a letter
 function handleCharacterIndex(){
     $('.characterIndex').click(event => {
         event.stopPropagation();
@@ -80,18 +113,25 @@ function handleCharacterIndex(){
         handleCardClick();
     });
 }
+
+//when a card is clicked, retrieve the name and img of hero, then run the super hero api
 function handleCardClick(){
     $('.card').click(event => {
         event.stopPropagation();
         let selectedCard = event.currentTarget;
         let cardName = $(selectedCard).text();
         let cardImg = $(selectedCard).find('img').prop('src');
-        //displayCharacterInfo(cardName, cardImg);
+        //used same convent from the modal guide on W3 schools
         document.getElementById('myModal').style.display = "none";
         runSuperHeroApi(cardName, cardImg);
     });
 }
+
+//runs when a hero is clicked in the modal
+//the url also contains the proxy server url along with the endpoint for the super hero API (CORS error)
 function runSuperHeroApi(cardName, cardImg){
+    //retrieves the exact name 
+    //iron man (Something) => iron man
     cardName = cardName.split(' (')[0];
     cardName = cardName.trim();
     
@@ -101,58 +141,73 @@ function runSuperHeroApi(cardName, cardImg){
         crossDomain: true,
         url: `https://sleepy-plateau-97577.herokuapp.com/http://www.superheroapi.com/api.php/2423459621001222/search/${cardName}`,
         success: function(data) {
-            //displayCharacterInfo(data, cardName, cardImg);
             console.log(data);
-            data.results.forEach(function(result){
-                console.log(result.name);
-                console.log(cardName);
-                if(result.name.toLowerCase() == cardName.toLowerCase()){
-                    displayCharacterInfo(result, cardName, cardImg);
-                }
-            });
-        },
-        error: function(jqXHR, textStatus, errorThrown) { 
-            console.log('jqXHR:');
-            console.log(jqXHR);
-            console.log('textStatus:');
-            console.log(textStatus);
-            console.log('errorThrown:');
-            console.log(errorThrown);
+            try {
+                //when retrieving the data, there would be some extra heros, this gets the exact one you clicked on.
+                //Ex search thor and it would return lex luTHOR 
+                data.results.forEach(function(result){
+                    if(result.name.toLowerCase() == cardName.toLowerCase()){
+                        displayCharacterInfo(result, cardName, cardImg);
+                    }
+                });
+            }
+            catch(err){
+                let characterLoadError = displaySuperHeroApiError(cardName, cardImg);
+                $('#characterPage').html(characterLoadError);
+            }
         }
+
       });
+    }
 
-
+//displays error message if something when wrong when a hero card was clicked
+function displaySuperHeroApiError(cardName, cardImg){
+    let errorMes = `<h3 class="errorName">${cardName}</h3>
+                    <img class = "characterThumbnail" src="${cardImg}">
+                    <div class="sorryTitleSection">
+                        <p class="sorryTitle">Sorry<p>
+                    </div>
+                    <p class="sorryDescription">There was an <span class="importantOpeningStatement">error</span> retrieving data!</p>
+                    <p class="sorryDescription">There is a possibility the Super Hero API does not have information on this hero. Hopefully they will update their API
+                    <span class="importantOpeningStatement"> SOON!!!</span></p>`;
+    return errorMes;
 }
 
-
+//displays the character information after you click on a hero card in the modal
 function displayCharacterInfo(data, characterName, characterImg){
     console.log(data);
-    let character = `<h3>${characterName}</h3><img src="${characterImg}">
+    let character = `<h3 class = "displayCharacterName">${characterName}</h3>
+                    <img class = "displayCharacterImage" src="${characterImg}">
                     <div class="row">
-                    <div class="col-4">
-                        <h3>Power Stats</h3>
-                        <p>Intelligence: ${data.powerstats.intelligence}</p>
-                        <p>Strength: ${data.powerstats.strength}</p>
-                        <p>Speed: ${data.powerstats.speed}</p>
-                        <p>Durability: ${data.powerstats.durability}</p>
-                        <p>Power: ${data.powerstats.power}</p>
-                        <p>Combat: ${data.powerstats.combat}</p>
+                    <div class="col-4 characterSection">
+                        <h3 class="characterSectionTitle">Power Stats</h3>
+                        <p class="characterSectionItems"><span class="sectionDescription">Intelligence: </span>${data.powerstats.intelligence}</p>
+                        <p class="characterSectionItems"><span class="sectionDescription">Strength: </span>${data.powerstats.strength}</p>
+                        <p class="characterSectionItems"><span class="sectionDescription">Speed: </span>${data.powerstats.speed}</p>
+                        <p class="characterSectionItems"><span class="sectionDescription">Durability: </span>${data.powerstats.durability}</p>
+                        <p class="characterSectionItems"><span class="sectionDescription">Power: </span>${data.powerstats.power}</p>
+                        <p class="characterSectionItems"><span class="sectionDescription">Combat: </span>${data.powerstats.combat}</p>
                     </div>
-                    <div class="col-4">
-                        <h3>Appearance</h3>
-                        <p>Gender: ${data.appearance.gender}</p>
-                        <p>Race: ${data.appearance.race}</p>
-                        <p>Height: ${data.appearance.height[0]}</p>
-                        <p>Weight: ${data.appearance.weight[0]}</p>
+                    <div class="col-4 characterSection">
+                        <h3 class="characterSectionTitle">Appearance</h3>
+                        <p class="characterSectionItems"><span class="sectionDescription">Gender: </span>${data.appearance.gender}</p>
+                        <p class="characterSectionItems"><span class="sectionDescription">Race: </span>${data.appearance.race}</p>
+                        <p class="characterSectionItems"><span class="sectionDescription">Height: </span>${data.appearance.height[0]}</p>
+                        <p class="characterSectionItems"><span class="sectionDescription">Weight: </span>${data.appearance.weight[0]}</p>
                     </div>
-                    <div class="col-4">
-                        <h3>Biography</h3>
-                        <p>${data.biography['full-name']}<p>
-                        <p>${data.biography.alignment}<p>
+                    <div class="col-4 characterSection">
+                        <h3 class="characterSectionTitle">Biography</h3>
+                        <p class="characterSectionItems"><span class="sectionDescription">Full Name: </span>${data.biography['full-name']}<p>
+                        <p class="characterSectionItems"><span class="sectionDescription">Alignment: </span>${data.biography.alignment}<p>
+                        <p class="characterSectionItems"><span class="sectionDescription">Place of Birth: </span>${data.biography['place-of-birth']}</p>
+                        <p class="characterSectionItems"><span class="sectionDescription">Alter Egos: </span>${data.biography['alter-egos']}</p>
                     </di>
                     </div>`;
     $('#characterPage').html(character);
 }
+
+//displays the A-Z index in the modal
+//characterIndexingList is found in characterObject.js
 function displayCharacterIndex(){
     $('#results').html(characterIndexingList);
     handleCharacterIndex();
@@ -169,6 +224,7 @@ function handleModal(){
     let span = document.getElementsByClassName("close")[0];
     // When the user clicks on the button, open the modal 
     btn.onclick = function() {
+        handleKeyDown();
         displayCharacterIndex();
         modal.style.display = "block";
     }
@@ -184,15 +240,16 @@ function handleModal(){
     }
 }
 
+//displays the opening remarks
+//openRemarks is a variable that is found in characterObject.js
+function displayOpeningRemarks(){
+    $('#characterPage').html(openingRemarks);
+}
 
-
-
-
-
+//starts the Application
 function startMarvelApi(){
-    fillCharacterObject()
-    handleKeyDown();
-    //handleCharacterIndex();
+    displayOpeningRemarks();
+    whenFillCharacter();
     displayCharacterIndex();
     handleCardClick();
     handleModal();
